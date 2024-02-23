01463fb6f0eff809f57aa4c8496e8500a2ebcfe8
Sials Detering <silas.detering@tu-dortmund.de>
2024-02-23 14:47:57 +0100

    Fragen melden und challenge random player hinzugefügt

--- bibleduel/routes/question_routes.py
+++ bibleduel/routes/question_routes.py
@@ -6,15 +6,10 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 def question_routes(app, db):
     question_service = QuestionService(db)
 
-    @app.route('/turn', methods=['GET'])
+    @app.route('/questions/<string:question_id>', methods=['GET'])
     @jwt_required()
-    def get_new_turn_data():
-        return question_service.get_new_turn_data()
-
-    @app.route('/categories', methods=['GET'])
-    @jwt_required()
-    def get_list_of_categories():
-        return question_service.get_list_of_categories()
+    def get_question(question_id):
+        return question_service.get_questions(question_id)
 
     @app.route('/questions', methods=['POST'])
     @jwt_required()
@@ -25,6 +20,22 @@ def question_routes(app, db):
         new_question = data.get('question')
         return question_service.add_question(user_id, new_question)
 
+    @app.route('/questions/<string:question_id>', methods=['DELETE'])
+    @jwt_required()
+    def delete_question(question_id):
+        user_id = get_jwt_identity()
+        return question_service.delete_question(user_id, question_id)
+
+    @app.route('/turn', methods=['GET'])
+    @jwt_required()
+    def get_new_turn_data():
+        return question_service.get_new_turn_data()
+
+    @app.route('/categories', methods=['GET'])
+    @jwt_required()
+    def get_list_of_categories():
+        return question_service.get_list_of_categories()
+
     @app.route('/categories', methods=['POST'])
     @jwt_required()
     def add_category():
@@ -33,22 +44,11 @@ def question_routes(app, db):
         new_category = data.get('category')
         return question_service.add_category(user_id, new_category)
 
-    @app.route('/questions/<string:question_id>', methods=['DELETE'])
-    @jwt_required()
-    def delete_question(question_id):
-        user_id = get_jwt_identity()
-        return question_service.delete_question(user_id, question_id)
-
     @app.route('/categories/<string:category>', methods=['DELETE'])
     @jwt_required()
     def delete_category(category):
         user_id = get_jwt_identity()
         return question_service.delete_category(user_id, category)
-
-    @app.route('/questions/<string:question_id>', methods=['GET'])
-    @jwt_required()
-    def get_question(question_id):
-        return question_service.get_questions(question_id)
 
     @app.route('/report', methods=['PUT'])
     @jwt_required()
@@ -57,5 +57,3 @@ def question_routes(app, db):
         data = request.json
         report = data.get('report')
         return question_service.report_question(user_id, report)
-
-
