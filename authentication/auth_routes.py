19cbdea128c64d28a040d3ad65f95c69599ae724
Sials Detering <silas.detering@tu-dortmund.de>
2024-03-04 14:57:33 +0100

    /api in auth routes hinzugefügt

--- authentication/auth_routes.py
+++ authentication/auth_routes.py
@@ -3,19 +3,18 @@ from flask import request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
-
 def auth_routes(app, db):
     auth_service = AuthService(db)
 
     @app.route('/user', methods=['POST'])
-    def register():
+    def register_2():
         username = request.json['username']
         password = request.json['password']
 
         return auth_service.register(username, password)
 
     @app.route('/user', methods=['PUT'])
-    def login():
+    def login_2():
         username = request.json['username']
         password = request.json['password']
 
@@ -23,11 +22,48 @@ def auth_routes(app, db):
 
     @app.route('/user', methods=['GET'])
     @jwt_required()
-    def refresh():
+    def refresh_2():
         user_id = get_jwt_identity()
         return auth_service.refresh(user_id)
 
     @app.route('/user/username', methods=['PUT'])
+    @jwt_required()
+    def change_username_2():
+        user_id = get_jwt_identity()
+        data = request.json
+        new_username = data.get('new_username')
+        return auth_service.change_username(new_username, user_id)
+
+    @app.route('/user/password', methods=['PUT'])
+    @jwt_required()
+    def change_password_2():
+        user_id = get_jwt_identity()
+        data = request.json
+        old_pw = data.get('old_password')
+        new_pw = data.get('new_password')
+        return auth_service.change_password(old_pw, new_pw, user_id)
+
+    @app.route('/api/user', methods=['POST'])
+    def register():
+        username = request.json['username']
+        password = request.json['password']
+
+        return auth_service.register(username, password)
+
+    @app.route('/api/user', methods=['PUT'])
+    def login():
+        username = request.json['username']
+        password = request.json['password']
+
+        return auth_service.login(username, password)
+
+    @app.route('/api/user', methods=['GET'])
+    @jwt_required()
+    def refresh():
+        user_id = get_jwt_identity()
+        return auth_service.refresh(user_id)
+
+    @app.route('/api/user/username', methods=['PUT'])
     @jwt_required()
     def change_username():
         user_id = get_jwt_identity()
@@ -35,7 +71,7 @@ def auth_routes(app, db):
         new_username = data.get('new_username')
         return auth_service.change_username(new_username, user_id)
 
-    @app.route('/user/password', methods=['PUT'])
+    @app.route('/api/user/password', methods=['PUT'])
     @jwt_required()
     def change_password():
         user_id = get_jwt_identity()
